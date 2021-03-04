3ec9fa3a933b8fed20c70ed83108d11273ef69a8
Anish <aa8143@nyu.edu>
2021-03-05 00:39:15 +0530

    Switch to html

--- src/GUI.py
+++ src/GUI.py
@@ -1,35 +1,8 @@
-import PySimpleGUI as sg
-from graph import generate_button
-# Define the window's contents
-# layout = [[sg.Text("Enter the number of nodes")],
-#           [sg.Input(key='nodes')],
-#           [sg.Text("Enter the number of edges")],
-#           [sg.Input(key='edges')],
-#   [sg.Button('Generate', key='generate_button')]]
-layout = [[sg.Column(
-    [[sg.Text("Enter the number of nodes")],
-     [sg.Input(key='nodes', size=(25, 10))]]),
-    sg.Column(
-    [[sg.Text("Enter the number of edges")],
-     [sg.Input(key='edges', size=(25, 10))]])
-],
-    [sg.Button('Generate', key='generate_button',
-               pad=((0, 0), (200, 0)))]
-]
-# Create the window
-sg.theme('DarkGrey12')
-window = sg.Window('Generate random graph', layout,
-                   size=(500, 500), element_justification='c')
+from flask import Flask, render_template
 
-while True:
-    event, values = window.read()
+app = Flask(__name__)
 
-    if event == sg.WINDOW_CLOSED:
-        break
 
-    window['generate_button'].update("regenerate graph")
-
-    if event == 'generate_button':
-        generate_button(values)
-
-window.close()
+@app.route('/')
+def main_page():
+    return render_template('index.html')
